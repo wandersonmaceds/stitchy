26d69b8c24cde7d8c92377eb4b9be305a2413d32
Wanderson M <wandersonmaceds@gmail.com>
2019-04-16 16:32:13 -0300

    added integration with slack and dotenv configurations

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
-const fs = require('fs');
-const path = require('path');
 const revisors = require('./revisors');
 const internalSupport = require('./internal-support');
+const axios = require('axios');
 
-const filePath = path.resolve(__dirname, 'data/sem-solucao.json');
-let posts = JSON.parse(fs.readFileSync(filePath, { encoding: 'UTF-8' })).list;
+require('dotenv').config();
+
+const slackAPIToken = process.env.SLACK_TOKEN;
+
+console.log(slackAPIToken);
 
 function buildMessage(user, topics){
-    const baseUrl = 'https://cursos.alura.com.br';
-    const mappedTopic = topics.map((topic, index) => `Tópico: ${index + 1}\nTempo de espera: ${topic.days} dias\nTítulo: ${topic.title}\nURL: ${baseUrl + topic.link}\n\n`)
-    return `Oi ${user}, separei esses tópicos pra gente ver hoje.\n\n${mappedTopic.join('')}`
+  const baseUrl = 'https://cursos.alura.com.br';
+  const mappedTopic = topics.map((topic, index) => `Tópico: ${index + 1}\nTempo de espera: ${topic.days} dias\nTítulo: ${topic.title}\nURL: ${baseUrl + topic.link}\n\n`)
+  return `Oi ${user}, separei esses tópicos pra gente ver hoje.\n\n${mappedTopic.join('')}`
 }
 
+function sendMessage(user, message){
+  axios.get('https://slack.com/api/chat.postMessage', {
+    params: {
+      token: slackAPIToken,
+      channel: user,
+      text: message,
+      username: 'Stitch',
+      icon_url: 'https://i.imgur.com/2WOJif0.png'
+    }
+  })
+  .then(response => console.log(response))
+  .catch(error => console.log(error));
+}
 
-internalSupport.forEach(user => {
-  const internalAlert = []
-  
-  user.courses.forEach(course => {
-      internalAlert.push(...(posts.filter(post => post.courseCode == course)));
-      posts = posts.filter(post => !internalAlert.includes(post))
-
-      if(internalAlert.length >= 10)
-        return;
-  });
-
-  if(internalAlert.length){
-    console.log(buildMessage(user.name, internalAlert));  
-  }
-})
+axios.get(process.env.FORUM_SEM_SOLUCAO_API)
+     .then(response => response.data.list)
+     .then(posts => {
+       internalSupport.forEach(user => {
+         const internalAlert = []
+         
+        user.courses.forEach(course => {
+          internalAlert.push(...(posts.filter(post => post.courseCode == course)));
+          posts = posts.filter(post => !internalAlert.includes(post))
+          
+          if(internalAlert.length >= 10)
+              return;
+            });
+            
+            if(internalAlert.length){
+              let message = buildMessage(user.name, internalAlert);
+              sendMessage(user.id, message);
+            }
+          });
+  })
+  .catch(error => console.log(error));
