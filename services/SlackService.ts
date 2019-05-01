152c76295ae703fc79fa654b74e0be60500c1b1c
Wanderson M <wandersonmaceds@gmail.com>
2019-05-01 15:19:54 -0300

    using HttpClient to send messages to slack and removind hard coded values

--- services/SlackService.ts
+++ services/SlackService.ts
@@ -1,22 +1,29 @@
-class SlackService{
+import { HttpClient } from "../helpers/HttpClient";
 
-    constructor(httpClient){
-        this._httpClient = httpClient;
-        this._slackTokenAPI = process.env.SLACK_TOKEN;
+class SlackService {
+    
+    private httpClient : HttpClient;
+    private slackToken : string;
+    private slackPostMessageURI: string;
+
+    constructor(httpClient : HttpClient){
+        this.httpClient = httpClient;
+        this.slackToken = process.env.SLACK_TOKEN;
+        this.slackPostMessageURI = process.env.SLACK_POST_MESSAGE_API;
     }
 
-    sendMessage(dest, message){
-        this._httpClient.get('https://slack.com/api/chat.postMessage', {
+    sendMessage(channel, message) : void {
+        this.httpClient.get(this.slackPostMessageURI, {
             params: {
-                token: this._slackTokenAPI,
-                channel: user,
+                token: this.slackToken,
+                channel: channel,
                 text: message,
-                username: 'Stitch',
-                icon_url: 'https://i.imgur.com/2WOJif0.png'
+                username: process.env.APP_NAME,
+                icon_url: process.env.APP_ICON_URL
             }
         })
         .then(() => {})
-        .catch(error => console.error(error));
+        .catch(error => console.error('Error while sending message to slack: ', error));
     }
     
 }
