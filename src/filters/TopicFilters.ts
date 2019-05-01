d2bbdda6a2aebd4960f5bb168183683b2437b01c
Wanderson M <wandersonmaceds@gmail.com>
2019-05-01 20:32:47 -0300

    created helper classe to filter posts to send to users

---
+++ src/filters/TopicFilters.ts
@@ -0,0 +1,19 @@
+import { Topic } from "../dto/Topic";
+
+export class TopicFilters{
+    
+    static filterByCourseCode(topics: Topic[], courseCode: string){
+        return topics.filter(topic => topic.courseCode == courseCode)
+    }
+
+    static filterByLimiter(topics: Topic[], limiter: number){
+        return topics.slice(0, limiter);
+    }
+
+    static filterByCoursesCodesAndLimiter(topics: Topic[], coursesCodes: [], limiter: number){
+        const topicsFiltered = [];
+        coursesCodes.forEach(courseCode => topicsFiltered.push(...this.filterByCourseCode(topics, courseCode)));
+        return this.filterByLimiter(topicsFiltered, limiter);
+    }
+
+}
