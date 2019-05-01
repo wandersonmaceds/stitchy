39c3eaef3aa5be79cd81556bf0d7d154e6efbbfb
Wanderson M <wandersonmaceds@gmail.com>
2019-05-01 15:57:39 -0300

    DTO of topics created to map response objects from the API inside the aplication

---
+++ dto/Topic.ts
@@ -0,0 +1,13 @@
+export interface Topic{
+    creationDate: Date;
+    days: number;
+    category: string;
+    subcategory: string;
+    course: string;
+    title: string;
+    link: string;
+    color: string;
+    categoryCode: string;
+    subcategoryCode: string;
+    courseCode: string;
+}
