5ddcd86f7e143b2be82284f756ad6a391e44d955
Wanderson M <wandersonmaceds@gmail.com>
2019-05-01 15:05:41 -0300

    change all .js files to .ts and removed unnecessary files

--- internal-support/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-    require('./guilherme'),
-    require('./felipe'),
-    require('./fabiano'),
-    require('./marina'),
-    require('./wanderson')
-]
--- revisors/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = [
-    require('./caio'),
-    require('./vasco'),
-]
