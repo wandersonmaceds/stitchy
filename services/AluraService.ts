9a226229eefdd9293daf62280e27119cc7bb620c
Wanderson M <wandersonmaceds@gmail.com>
2019-05-01 15:26:24 -0300

    using httpClient on AluraService to get no answered topics from the API

--- services/AluraService.ts
+++ services/AluraService.ts
@@ -1,12 +1,16 @@
+import { HttpClient } from "../helpers/HttpClient";
+
 class AluraService{
     
-    constructor(httpClient){
-        this._httpClient = httpClient;
+    private httpClient: HttpClient;
+
+    constructor(httpClient: HttpClient){
+        this.httpClient = httpClient;
     }
 
     async getNoAnsweredTopics() {
-        await this._httpClient.get(process.env.FORUM_CLEAN_CACHE);
-        const response = await this._httpClient.get(process.env.FORUM_SEM_RESPOSTAS_API)
+        await this.httpClient.get(process.env.FORUM_CLEAN_CACHE);
+        const response = await this.httpClient.get(process.env.FORUM_SEM_RESPOSTAS_API)
         return response.data.list;
     }
 }
